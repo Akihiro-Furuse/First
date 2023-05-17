0f3525322f76e8ddddaf3bf2b62049df80c125de
b062ff <zom2MAT3@outlook.jp>
2023-05-17 22:26:25 +0900

    変数xを追加

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main(void) {
 	int a,b;
 	int c;
+	int x;
 	printf("数字を1つ入力してください");
 	scanf_s("%d", &a);
 	printf("数字をもう1つ入力してください");
@@ -10,6 +11,5 @@ int main(void) {
 	printf("入力された数字の合計はは%dです", a+b);
 	printf("かける数字をもう1つ入力してください");
 	scanf_s("%d", &c);
-	printf("入力された数字の合計に入力した値をかけた数は%dです", (a + b)*c);
-	return (0);
+	printf("入力された数字の合計に入力した値をかけた数は%dです", (	return (0);
 }

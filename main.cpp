d94c45bbde0a0de78ba1ad1a8ba3de15378cf6e4
unknown <d075vgu@yamaguchi-u.ac.jp>
2023-05-17 19:25:58 +0900

    Add Program

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-	int a;
+	int a,b;
 	printf("”š‚ğ1‚Â“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
 	scanf_s("%d", &a);
-	printf("“ü—Í‚³‚ê‚½”š‚Í%d‚Å‚·", a);
+	printf("”š‚ğ‚à‚¤1‚Â“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+	scanf_s("%d", &b);
+	printf("“ü—Í‚³‚ê‚½”š‚Ì‡Œv‚Í‚Í%d‚Å‚·", a+b);
 	return (0);
 }
